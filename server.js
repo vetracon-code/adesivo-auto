46d494a92f69613b7689f2dd32d8902265cf80eb
Vetracon <vetracon@gmail.com>
2026-05-22 16:43:48 +0200

    avoid cache on public image save link

--- server.js
+++ server.js
@@ -10794,7 +10794,7 @@ app.get('/fm/image/:code', async (req, res) => {
         <img src="${esc(imageUrl)}?v=${Date.now()}" alt="${title}">
       </div>
 
-      <a class="followme-public-image-download-btn-20260522" id="followmePublicImageDownloadBtn20260522" href="${esc(imageUrl)}" data-original-image-url="${esc(imageUrl)}" download target="_blank" rel="noopener">
+      <a class="followme-public-image-download-btn-20260522" id="followmePublicImageDownloadBtn20260522" href="${esc(imageUrl)}?v=${img.id}" data-original-image-url="${esc(imageUrl)}?v=${img.id}" download target="_blank" rel="noopener">
         <svg viewBox="0 0 24 24" aria-hidden="true">
           <path d="M12 3v10.2m0 0 4-4m-4 4-4-4M5 17.5V20h14v-2.5" fill="none" stroke="currentColor" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
